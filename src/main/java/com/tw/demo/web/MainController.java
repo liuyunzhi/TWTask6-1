18fb860d08b5ad1c8004b20aff667d32a967893c
刘云志 <liuyunzhi@chinamcloud.com>
2018-05-03 15:38:15 +0800

    调整逻辑，实现增加和删除的API接口

--- src/main/java/com/tw/demo/web/MainController.java
+++ src/main/java/com/tw/demo/web/MainController.java
@@ -1,22 +1,39 @@
 package com.tw.demo.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/employees")
 public class MainController {
 
-    @RequestMapping("/employees")
-    public Employees[] index() {
-        Employees employee0 = new Employees(0, "小明", 20, "男");
-        Employees employee1 = new Employees(1, "小红", 19, "女");
-        Employees employee2 = new Employees(2, "小智", 15, "男");
-        Employees employee3 = new Employees(3, "小刚", 16, "男");
-        Employees employee4 = new Employees(4, "小霞", 15, "女");
+    private List<Employees> _employees = new ArrayList<>();
 
-        Employees[] result = {employee0, employee1, employee2, employee3, employee4};
+    private List<Employees> initial() {
+        _employees.add(new Employees(0, "小明", 20, "男"));
+        _employees.add(new Employees(1, "小红", 19, "女"));
+        _employees.add(new Employees(2, "小智", 15, "男"));
+        _employees.add(new Employees(3, "小刚", 16, "男"));
+        _employees.add(new Employees(4, "小霞", 15, "女"));
 
-        return result;
+        return _employees;
+    }
+
+    @GetMapping("/read")
+    public List<Employees> read() {
+        return initial();
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestParam("id") Integer id, @RequestParam("name") String name, @RequestParam("age") Integer age, @RequestParam("gender") String gender){
+        _employees.add(new Employees(id, name, age, gender));
+    }
+
+    @GetMapping("/delete")
+    public void delete(@RequestParam("id") Integer id){
+        _employees.remove(id);
     }
 
 }
